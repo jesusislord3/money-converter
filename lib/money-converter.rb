da26cd7f965b9e9e0a9e40b323282a3fda4a9e3e
LEONEL MORAIS <leonel.morais@yahoo.com.br>
2012-11-30 01:05:03 -0200

    Removing to_currency param from format_amount_exchanged method

--- lib/money-converter.rb
+++ lib/money-converter.rb
@@ -22,15 +22,15 @@ class MoneyConverter
 		http = Net::HTTP.new("www.google.com",80)
 		resp = http.get("/ig/calculator?hl=en&q=#{amount}#{from_currency}=?#{to_currency}")
 		data = resp.body
-		format_amount_exchanged(data, to_currency)		
+		format_amount_exchanged(data)		
 		#result = JSON(r.gsub(/([\w]+):/, '"\1":'))
 	end
 
-	def format_amount_exchanged(data, to_currency)
+	def format_amount_exchanged(data)
 		array = data.split('"')
 		amount_exchanged = array[3].split
 		result = amount_exchanged[0].gsub(/[\xA0]/n, '')
-		"#{result} #{to_currency}".to_sym
+		"#{result} #{@to_currency}".to_sym
 	end
 
 end
